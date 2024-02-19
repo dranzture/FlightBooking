fd852adfd08f76dc041c28897e9645d95bbf0b5f
dranzture <polatcoban@gmail.com>
2024-02-18 23:31:58 -0600

    Missing migrations

---
+++ Training.FlightBooking.Infrastructure/Data/Migrations/20240219041949_ChangeColumnNameSeatsFlights.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Training.IntegrationTest.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeColumnNameSeatsFlights : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "AvailableSeats",
+                table: "Flights",
+                newName: "Seats");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Seats",
+                table: "Flights",
+                newName: "AvailableSeats");
+        }
+    }
+}
